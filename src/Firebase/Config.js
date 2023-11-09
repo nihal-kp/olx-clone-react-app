274ae42e929f9e1e86bf71060d54f56207a607bb
Nihal K P <nihal44.kp@gmail.com>
2023-11-09 23:52:21 +0530

    First Commit

---
+++ src/Firebase/Config.js
@@ -0,0 +1,13 @@
+import { initializeApp } from 'firebase/app';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyDkgYu17YOEsIPXfIQqC3n0sPtQuRTeQM0",
+    authDomain: "olx-clone-project-82c64.firebaseapp.com",
+    projectId: "olx-clone-project-82c64",
+    storageBucket: "olx-clone-project-82c64.appspot.com",
+    messagingSenderId: "975942207167",
+    appId: "1:975942207167:web:e1f0d1d38ab66e8585680a",
+    measurementId: "G-K7SRR2C9BG"
+  };
+
+  export default initializeApp(firebaseConfig);
